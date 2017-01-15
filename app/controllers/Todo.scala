9de88db44bf14740e395af1e53359d1a3be5a939
berczeck <berczeck69@gmail.com>
2017-01-15 09:56:36 -0500

    json request validated

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -1,9 +1,9 @@
 package controllers
 
 import akka.util.ByteString
-import models.{ TodoModel, TodoRepository}
+import models.{TodoModel, TodoRepository}
 import play.api.http.HttpEntity
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.mvc._
 
 /**
@@ -23,12 +23,19 @@ class Todo extends Controller {
   def post() = Action { request =>
     //http://alvinalexander.com/scala/how-to-write-play-framework-http-post-request-json-web-service
     val json = request.body.asJson.get
-    val todo: TodoModel = json.as[TodoModel].copy(url = s"${request.path}")
-    val model: TodoModel = repo.save(todo)
-    Result(
-      header = ResponseHeader(200, Map("location" -> model.url)),
-      body = HttpEntity.Strict(ByteString(Json.toJson(model).toString()), Some("application/json"))
-    )
+
+    //https://www.playframework.com/documentation/2.5.x/ScalaJson
+    json.validate[TodoModel] match {
+      case s: JsSuccess[TodoModel] =>
+        val todo = s.get.copy(url = s"${request.path}")
+        val model: TodoModel = repo.save(todo)
+        Result(
+          header = ResponseHeader(200, Map("location" -> model.url)),
+          body = HttpEntity.Strict(ByteString(Json.toJson(model).toString()), Some("application/json"))
+        )
+      case _: JsError =>
+        BadRequest("Bad request")
+    }
   }
 
   def patch(id:Int) = Action { request =>
